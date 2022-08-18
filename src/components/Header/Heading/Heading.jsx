ba3e22fffc829d682f9e02661cc21f6dd12303c2
Oksana-png <oksana_ovchinnikova@inbox.ru>
2022-08-18 23:55:25 +0500

    push all failes

---
+++ src/components/Header/Heading/Heading.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+import style from './Heading.module.css';
+
+export const Heading = ({text}) =>
+  <h1 className={style.heading}>
+    {text}
+  </h1>;
+
+Heading.propTypes = {
+  text: PropTypes.string,
+};
---
+++ src/components/Header/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import style from './Logo.module.css';
+import logo from './img/logo.svg';
+
+export const Logo = props =>
+  <a href="/" className={style.link}>
+    <img className={style.logo} src={logo} alt="logo"></img>
+  </a>;
